b714a85b0d745830c8b54c2666ea0e703ed6685b
guilhermeisnard07 <guilherme.isnard07@gmail.com>
2026-07-18 13:49:55 -0300

    Atualização v2

--- JavaScrip/main.js
+++ JavaScrip/main.js
@@ -1,4 +1,24 @@
+// Preloader //
 window.addEventListener("load", function() {
     const preloader = document.getElementById("tela-de-carregamento");
     preloader.classList.add("preloader-hidden");
+});
+
+// slide feedback //
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector('.slide-feedback');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    const scrollAmount = 948;
+
+    if (container && nextBtn && prevBtn) {
+        nextBtn.addEventListener('click', () => {
+            container.scrollBy({ left: 320, behavior: 'smooth' });
+        });
+
+        prevBtn.addEventListener('click', () => {
+            container.scrollBy({ left: -320, behavior: 'smooth' });
+        });
+    }
 });
